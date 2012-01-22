b155cab83513ee7c6cd49ead22b791c9ad4078f4
lechuckcaptain@gmail.com <lechuckcaptain@gmail.com@13554efa-3f24-54eb-d928-8472ff44cb35>
2012-01-22 10:23:22 +0000

    Added some proxy utility methods

---
+++ AndroidProxyLibrary/src/com/lechucksoftware/proxy/lib/ProxyUtils.java
@@ -0,0 +1,70 @@
+package com.lechucksoftware.proxy.lib;
+
+import java.io.IOException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.params.ConnRoutePNames;
+import org.apache.http.impl.client.DefaultHttpClient;
+import android.util.Log;
+
+public class ProxyUtils
+{
+	public static final String TAG = "ProxyUtils";
+	
+	public static boolean isSystemProxyReachable(HttpHost proxy)
+	{
+		int exitValue;
+		Runtime runtime = Runtime.getRuntime();
+		Process proc;
+
+		try {
+			proc = runtime.exec("ping -c 1   " + proxy.getHostName());
+			proc.waitFor();
+			exitValue = proc.exitValue();
+
+			Log.d(TAG, "Ping exit value: " + exitValue);
+
+			if (exitValue == 0)
+				return true;
+			else
+				return false;
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	public static boolean isInternetReachable(HttpHost proxy)
+	{
+		DefaultHttpClient httpclient = new DefaultHttpClient();
+		httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,
+				proxy);
+
+		HttpGet request;
+		HttpResponse response;
+
+		try {
+			request = new HttpGet("http://www.google.com");
+			response = httpclient.execute(request);
+
+			Log.d(TAG, "Is internet reachable : "
+					+ response.getStatusLine().toString());
+			if (response != null
+					&& response.getStatusLine().getStatusCode() == 200) {
+				return true;
+			} else
+				return false;
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+}
