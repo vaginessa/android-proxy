dd4d241d02f741ff6ebe5e0e827e3ac99d202359
lechuckcaptain <lechuckcaptain@13554efa-3f24-54eb-d928-8472ff44cb35>
2012-08-26 14:55:42 +0000

    Added utilities to check the goodness of the current proxy configuration

---
+++ AndroidProxyLibrary/src/com/lechucksoftware/proxy/lib/Constants.java
@@ -0,0 +1,14 @@
+package com.lechucksoftware.proxy.lib;
+
+public class Constants
+{
+	public enum ProxyStatus
+	{
+		NOT_CHECKED,
+		PROXY_NOT_ENABLED,
+		PROXY_INVALID_ADDRESS,
+		PROXY_NOT_REACHABLE,
+		WEB_NOT_REACHABLE,
+		OK
+	}
+}
--- AndroidProxyLibrary/src/com/lechucksoftware/proxy/lib/ProxyConfiguration.java
+++ AndroidProxyLibrary/src/com/lechucksoftware/proxy/lib/ProxyConfiguration.java
@@ -4,14 +4,20 @@ import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.Proxy.Type;
 
+import org.apache.http.conn.util.InetAddressUtils;
+
+import com.lechucksoftware.proxy.lib.Constants.ProxyStatus;
+
 import android.net.NetworkInfo;
 import android.net.wifi.WifiConfiguration;
 import android.os.Build;
+import android.webkit.URLUtil;
 
 public class ProxyConfiguration
-{
+{	
 	public static final String TAG = "ProxyConfiguration";
 
+	public ProxyStatus status;
 	public WifiConfiguration wifiConfiguration;
 	public NetworkInfo networkInfo;
 	public Proxy proxyHost;
@@ -23,6 +29,7 @@ public class ProxyConfiguration
 		networkInfo = netInfo;
 		wifiConfiguration = wifiConf;
 		deviceVersion = Build.VERSION.SDK_INT;
+		status = ProxyStatus.NOT_CHECKED;
 	}
 	
 	@Override
@@ -44,6 +51,40 @@ public class ProxyConfiguration
 		return proxyHost.type();
 	}
 	
+	public void acquireProxyStatus()
+	{
+		if (!isProxyEnabled())
+		{
+			status = ProxyStatus.PROXY_NOT_ENABLED;
+			return;
+		}
+		
+		if (!isProxyValidAddress())
+		{
+			status = ProxyStatus.PROXY_INVALID_ADDRESS;
+			return;
+		}
+		
+		if (!isProxyReachable())
+		{
+			status = ProxyStatus.PROXY_NOT_REACHABLE;
+			return;
+		}
+		
+		if (!isWebReachable())
+		{
+			status = ProxyStatus.WEB_NOT_REACHABLE;
+			return;
+		}
+		
+		status = ProxyStatus.OK;
+	}
+
+	public ProxyStatus getProxyStatus()
+	{
+		return status;
+	}
+	
 	public Boolean isProxyEnabled()
 	{
 		if (proxyHost.type() == Type.DIRECT)
@@ -56,6 +97,29 @@ public class ProxyConfiguration
 		}
 	}
 	
+	private boolean isProxyValidAddress()
+	{
+		String proxyHost = getProxyHost();
+		
+		if (InetAddressUtils.isIPv4Address(proxyHost) || 
+			InetAddressUtils.isIPv6Address(proxyHost) || 
+			InetAddressUtils.isIPv6HexCompressedAddress(proxyHost) || 
+			InetAddressUtils.isIPv6StdAddress(proxyHost))
+		{
+			return true;
+		}
+		
+		if (URLUtil.isNetworkUrl(proxyHost))
+		{
+			return true;
+		}
+
+		return false;
+	}
+	
+	/**
+	 * Try to PING the HOST specified in the current proxy configuration
+	 * */ 
 	public Boolean isProxyReachable()
     {
 	    if (proxyHost != null && proxyHost.type() != Proxy.Type.DIRECT)
@@ -64,6 +128,9 @@ public class ProxyConfiguration
 	        return false;
     }
 	
+	/**
+	 * Try to download a webpage using the current proxy configuration
+	 * */
 	public Boolean isWebReachable()
     {
         return ProxyUtils.isWebReachable(proxyHost);
