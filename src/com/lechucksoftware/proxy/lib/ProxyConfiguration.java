a11c4dfddc47e5cfec968dd0df3e8d1b68235d18
lechuckcaptain <lechuckcaptain@shouldit.net>
2012-08-30 21:53:52 +0200

    Cleaning up folders

---
+++ src/com/lechucksoftware/proxy/lib/ProxyConfiguration.java
@@ -0,0 +1,171 @@
+package com.lechucksoftware.proxy.lib;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+
+import org.apache.http.conn.util.InetAddressUtils;
+
+import com.lechucksoftware.proxy.lib.Constants.ProxyStatus;
+
+import android.net.NetworkInfo;
+import android.net.wifi.WifiConfiguration;
+import android.os.Build;
+import android.webkit.URLUtil;
+
+public class ProxyConfiguration
+{	
+	public static final String TAG = "ProxyConfiguration";
+
+	public ProxyStatus status;
+	public WifiConfiguration wifiConfiguration;
+	public NetworkInfo networkInfo;
+	public Proxy proxyHost;
+	public int deviceVersion;
+
+	public ProxyConfiguration(Proxy proxy, NetworkInfo netInfo, WifiConfiguration wifiConf)
+	{
+		proxyHost = proxy;
+		networkInfo = netInfo;
+		wifiConfiguration = wifiConf;
+		deviceVersion = Build.VERSION.SDK_INT;
+		status = ProxyStatus.NOT_CHECKED;
+	}
+	
+	@Override
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder(); 
+		sb.append(String.format("Proxy: %s\n",proxyHost.toString()));
+		sb.append(String.format("Is Proxy reachable: %B\n",isProxyReachable()));
+		sb.append(String.format("Is WEB reachable: %B\n",isWebReachable()));
+		
+		if (networkInfo != null) sb.append(String.format("Network Info: %s\n", networkInfo));
+		if (wifiConfiguration != null) sb.append(String.format("Wi-Fi Configuration Info: %s\n", wifiConfiguration));
+		
+		return sb.toString();
+	}
+	
+	public Proxy.Type getConnectionType()
+	{
+		return proxyHost.type();
+	}
+	
+	public void acquireProxyStatus()
+	{
+		if (!isProxyEnabled())
+		{
+			status = ProxyStatus.PROXY_NOT_ENABLED;
+			return;
+		}
+		
+		if (!isProxyValidAddress())
+		{
+			status = ProxyStatus.PROXY_INVALID_ADDRESS;
+			return;
+		}
+		
+		if (!isProxyReachable())
+		{
+			status = ProxyStatus.PROXY_NOT_REACHABLE;
+			return;
+		}
+		
+		if (!isWebReachable())
+		{
+			status = ProxyStatus.WEB_NOT_REACHABLE;
+			return;
+		}
+		
+		status = ProxyStatus.OK;
+	}
+
+	public ProxyStatus getProxyStatus()
+	{
+		return status;
+	}
+	
+	public Boolean isProxyEnabled()
+	{
+		if (proxyHost.type() == Type.DIRECT)
+		{
+			return false;
+		}
+		else
+		{
+			return true; // HTTP or SOCKS proxy
+		}
+	}
+	
+	private boolean isProxyValidAddress()
+	{
+		String proxyHost = getProxyHost();
+		
+		if (InetAddressUtils.isIPv4Address(proxyHost) || 
+			InetAddressUtils.isIPv6Address(proxyHost) || 
+			InetAddressUtils.isIPv6HexCompressedAddress(proxyHost) || 
+			InetAddressUtils.isIPv6StdAddress(proxyHost))
+		{
+			return true;
+		}
+		
+		if (URLUtil.isNetworkUrl(proxyHost))
+		{
+			return true;
+		}
+
+		return false;
+	}
+	
+	/**
+	 * Try to PING the HOST specified in the current proxy configuration
+	 * */ 
+	public Boolean isProxyReachable()
+    {
+	    if (proxyHost != null && proxyHost.type() != Proxy.Type.DIRECT)
+	        return ProxyUtils.isHostReachable(proxyHost);
+	    else
+	        return false;
+    }
+	
+	/**
+	 * Try to download a webpage using the current proxy configuration
+	 * */
+	public Boolean isWebReachable()
+    {
+        return ProxyUtils.isWebReachable(proxyHost);
+    }
+	
+	public String getProxyHost()
+	{
+		InetSocketAddress proxyAddress = (InetSocketAddress) proxyHost.address();
+		return proxyAddress.getHostName();
+	}
+	
+	public String getProxyIPHost()
+	{
+	    return ((InetSocketAddress) proxyHost.address()).getAddress().getHostAddress();
+	}
+	
+	public Integer getProxyPort()
+	{
+		InetSocketAddress proxyAddress = (InetSocketAddress) proxyHost.address();
+		return proxyAddress.getPort();
+	}
+	
+	public String toShortString()
+	{
+		return String.format("%s",proxyHost.address().toString());
+	}
+
+	public String toShortIPString()
+    {
+        return String.format("%s:%d",getProxyIPHost(),getProxyPort());
+    }
+	
+	
+	public int getNetworkType()
+	{
+		return networkInfo.getType();
+	}
+}
---
+++ src/com/lechucksoftware/proxy/lib/ProxySettings.java
@@ -0,0 +1,170 @@
+package com.lechucksoftware.proxy.lib;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.Proxy.Type;
+import java.util.List;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.util.Log;
+
+/**
+ * Main class that contains utilities for getting the proxy configuration of the
+ * current or the all configured networks
+ * */
+public class ProxySettings
+{
+	public static final String TAG = "ProxySettings";
+
+	/**
+	 * Main entry point to access the proxy settings
+	 * */
+	public static ProxyConfiguration getCurrentProxyConfiguration(Context ctx, URI uri) throws Exception
+    {
+		ProxyConfiguration proxyConfig;
+		
+      	if (Build.VERSION.SDK_INT >= 12) // Honeycomb 3.1 
+    	{
+      		proxyConfig = getProxySelectorConfiguration(ctx, uri);
+		}
+    	else
+    	{
+    		proxyConfig = getGlobalProxy(ctx);
+    	}	
+      	
+      	/**
+      	 * Set direct connection if no proxyConfig received
+      	 * */
+      	if (proxyConfig == null) 
+		{
+			proxyConfig = new ProxyConfiguration(Proxy.NO_PROXY,null,null);
+		} 
+      	
+      	/**
+      	 * Add connection informations
+      	 * */
+		ConnectivityManager connManager = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetInfo = connManager.getActiveNetworkInfo();
+		proxyConfig.networkInfo = activeNetInfo;
+		
+		switch (activeNetInfo.getType())
+		{
+			case ConnectivityManager.TYPE_WIFI:
+				WifiManager wifiManager = (WifiManager) ctx.getSystemService(Context.WIFI_SERVICE);
+				WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+				List<WifiConfiguration> wifiConfigurations = wifiManager.getConfiguredNetworks();
+				for(WifiConfiguration wc:wifiConfigurations)
+				{
+					if (wc.networkId == wifiInfo.getNetworkId())
+					{
+						proxyConfig.wifiConfiguration = wc;
+						break;
+					}
+				}
+				break;
+			case ConnectivityManager.TYPE_MOBILE:
+				break;
+			default:
+				throw new UnsupportedOperationException("Not yet implemented support for" + activeNetInfo.getTypeName() + " network type");
+		}
+		
+		return proxyConfig;
+    }
+
+	/**
+	 * For API >= 12: Returns the current proxy configuration based on the URI, this
+	 * implementation is a wrapper of the Android's ProxySelector class. Just
+	 * add some other informations that can be useful to the developer. 
+	 * */
+	public static ProxyConfiguration getProxySelectorConfiguration(Context ctx, URI uri) throws Exception
+	{
+		ProxySelector defaultProxySelector = ProxySelector.getDefault();
+
+		Proxy proxy = null;
+
+		List<Proxy> proxyList = defaultProxySelector.select(uri);
+		if (proxyList.size() > 0)
+		{
+			proxy = proxyList.get(0);
+			Log.d(TAG, "Current Proxy Configuration: " + proxy.toString());
+		}
+		else throw new Exception("Not found valid proxy configuration!");
+
+		ConnectivityManager connManager = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetInfo = connManager.getActiveNetworkInfo();
+		ProxyConfiguration proxyConfig = new ProxyConfiguration(proxy, activeNetInfo, null);
+
+		return proxyConfig;
+	}
+
+	/**
+	 * Return the current proxy configuration for HTTP protocol
+	 * */
+	public static ProxyConfiguration getCurrentHttpProxyConfiguration(Context ctx) throws Exception
+	{
+		URI uri = new URI("http", "wwww.google.it", null, null);
+		return getCurrentProxyConfiguration(ctx, uri);
+	}
+
+	/**
+	 * Return the current proxy configuration for HTTPS protocol
+	 * */
+	public static ProxyConfiguration getCurrentHttpsProxyConfiguration(Context ctx) throws Exception
+	{
+		URI uri = new URI("https", "wwww.google.it", null, null);
+		return getCurrentProxyConfiguration(ctx, uri);
+	}
+
+	/**
+	 * Return the current proxy configuration for FTP protocol
+	 * */
+	public static ProxyConfiguration getCurrentFtpProxyConfiguration(Context ctx) throws Exception
+	{
+		URI uri = new URI("ftp", "google.it", null, null);
+		return getCurrentProxyConfiguration(ctx, uri);
+	}
+
+	
+	/**
+	 * For API < 12: Get global proxy configuration. 
+	 * */
+	private static ProxyConfiguration getGlobalProxy(Context ctx)
+	{
+		ProxyConfiguration proxyConfig = null;
+
+		ContentResolver contentResolver = ctx.getContentResolver();
+		String proxyString = Settings.Secure.getString(contentResolver, Settings.Secure.HTTP_PROXY);
+
+		if (proxyString != null && proxyString != "" && proxyString.contains(":"))
+		{
+			String[] proxyParts = proxyString.split(":");
+			if (proxyParts.length == 2)
+			{
+				String proxyAddress = proxyParts[0];
+				try
+				{
+					Integer proxyPort = Integer.parseInt(proxyParts[1]);
+					Proxy p = new Proxy(Type.HTTP,new InetSocketAddress(proxyAddress,proxyPort));
+					proxyConfig = new ProxyConfiguration(p, null, null);
+					//Log.d(TAG, "ProxyHost created: " + proxyConfig.toString());
+				}
+				catch (NumberFormatException e)
+				{
+					Log.d(TAG, "Port is not a number: " + proxyParts[1]);
+				}
+			}
+		}
+
+		return proxyConfig;
+	}
+}
