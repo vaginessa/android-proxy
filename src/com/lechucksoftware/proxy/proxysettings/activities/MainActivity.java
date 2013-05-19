91d6deca42f1adca768a4841bb2a9ad906baebcb
lechuckcaptain <lechuckcaptain@shouldit.net>
2013-05-19 17:00:57 +0200

    Fixed refresh of proxy details fragment, based on proxy configuration changes

--- src/com/lechucksoftware/proxy/proxysettings/activities/MainActivity.java
+++ src/com/lechucksoftware/proxy/proxysettings/activities/MainActivity.java
@@ -3,10 +3,19 @@ package com.lechucksoftware.proxy.proxysettings.activities;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import com.lechucksoftware.proxy.proxysettings.R;
 import com.lechucksoftware.proxy.proxysettings.fragments.*;
+import com.lechucksoftware.proxy.proxysettings.services.ViewServer;
+import com.lechucksoftware.proxy.proxysettings.utils.LogWrapper;
+import com.shouldit.proxy.lib.APLConstants;
 
 
 /**
@@ -14,10 +23,12 @@ import com.lechucksoftware.proxy.proxysettings.fragments.*;
  */
 public class MainActivity extends Activity
 {
-    private ProxyDetailsFragment mainFragment;
-    private HelpPrefsFragment helpFragment;
-    private ProxyCheckerPrefsFragment checkFragment;
-    private AdvancedPrefsFragment advFragment;
+    public static final String TAG = "MainActivity";
+
+//    private ProxyDetailsFragment mainFragment;
+//    private HelpPrefsFragment helpFragment;
+//    private ProxyCheckerPrefsFragment checkFragment;
+//    private AdvancedPrefsFragment advFragment;
     private AccessPointListFragment apSelectorFragment;
 
     @Override
@@ -27,10 +38,10 @@ public class MainActivity extends Activity
 
         setContentView(R.layout.main_layout);
 
-        mainFragment = new ProxyDetailsFragment();
-        checkFragment = new ProxyCheckerPrefsFragment();
-        advFragment = new AdvancedPrefsFragment();
-        helpFragment = new HelpPrefsFragment();
+//        mainFragment = new ProxyDetailsFragment();
+//        checkFragment = new ProxyCheckerPrefsFragment();
+//        advFragment = new AdvancedPrefsFragment();
+//        helpFragment = new HelpPrefsFragment();
         apSelectorFragment = new AccessPointListFragment();
 
         // Check that the activity is using the layout version with
@@ -48,8 +59,6 @@ public class MainActivity extends Activity
             // Add the fragment to the 'fragment_container' FrameLayout
             getFragmentManager().beginTransaction().add(R.id.fragment_container,apSelectorFragment) .commit();
         }
-
-
     }
 
     @Override
@@ -71,5 +80,103 @@ public class MainActivity extends Activity
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+
+    public void onDestroy()
+    {
+        super.onDestroy();
+        ViewServer.get(this).removeWindow(this);
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        // Start register the status receivers
+        IntentFilter ifilt = new IntentFilter();
+
+        ifilt.addAction(APLConstants.APL_UPDATED_PROXY_CONFIGURATION);
+        ifilt.addAction(APLConstants.APL_UPDATED_PROXY_STATUS_CHECK);
+
+        ifilt.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        ifilt.addAction(WifiManager.SUPPLICANT_CONNECTION_CHANGE_ACTION);
+        ifilt.addAction(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
+        ifilt.addAction(WifiManager.WIFI_STATE_CHANGED_ACTION);
+//		ifilt.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+
+        // ifilt.addAction(Constants.PROXY_REFRESH_UI);
+        registerReceiver(changeStatusReceiver, ifilt);
+
+        ViewServer.get(this).setFocusedWindow(this);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+
+        // Stop the registered status receivers
+        unregisterReceiver(changeStatusReceiver);
+    }
+
+    private BroadcastReceiver changeStatusReceiver = new BroadcastReceiver()
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            String action = intent.getAction();
+            if (action.equals(APLConstants.APL_UPDATED_PROXY_CONFIGURATION))
+            {
+                LogWrapper.d(TAG, "Received broadcast for updated proxy configuration - RefreshUI");
+                refreshUI();
+            }
+            else if (action.equals(APLConstants.APL_UPDATED_PROXY_STATUS_CHECK))
+            {
+                LogWrapper.d(TAG, "Received broadcast for partial update to proxy configuration - RefreshUI");
+                refreshUI();
+            }
+            else if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION) || action.equals(WifiManager.SUPPLICANT_CONNECTION_CHANGE_ACTION) || action.equals(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION) || action.equals(WifiManager.WIFI_STATE_CHANGED_ACTION))
+            {
+                LogWrapper.logIntent(TAG, intent, Log.DEBUG, true);
+                refreshUI();
+            }
+            else
+            {
+                LogWrapper.e(TAG, "Received intent not handled: " + intent.getAction());
+            }
+        }
+    };
+
+    private void refreshUI()
+    {
+        this.invalidateOptionsMenu();
+
+        if (ProxyDetailsFragment.getInstance().isVisible())
+        {
+            ProxyDetailsFragment.getInstance().refreshUIComponents();
+        }
+
+//        if (checkFragment.isVisible())
+//        {
+//            checkFragment.refreshUIComponents();
+//        }
+    }
+
+    static boolean active = false;
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        active = true;
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+        active = false;
     }
 }
