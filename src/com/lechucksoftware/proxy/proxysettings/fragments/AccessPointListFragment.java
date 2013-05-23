7a5fd6feffe381d474f0a4d873e3e1d29fb46ccb
lechuckcaptain <lechuckcaptain@shouldit.net>
2013-05-23 21:21:52 +0200

    Deleted loader part. Now seems to work (evaluate about enabling again in future)

--- src/com/lechucksoftware/proxy/proxysettings/fragments/AccessPointListFragment.java
+++ src/com/lechucksoftware/proxy/proxysettings/fragments/AccessPointListFragment.java
@@ -23,79 +23,18 @@ import java.util.List;
 /**
  * Created by marco on 17/05/13.
  */
-public class AccessPointListFragment extends ListFragment implements LoaderManager.LoaderCallbacks<List<ProxyConfiguration>>
-{
+public class AccessPointListFragment extends ListFragment {
     private static AccessPointListFragment instance;
-    boolean mDualPane;
     int mCurCheckPosition = 0;
     private ProxySelectorListAdapter apListAdapter;
 
-    public ListView mList;
-    boolean mListShown;
-    View mProgressContainer;
-    View mListContainer;
-
-    public void setListShown(boolean shown, boolean animate)
-    {
-        if (mListShown == shown)
-        {
-            return;
-        }
-
-        mListShown = shown;
-
-        if (shown)
-        {
-            if (animate)
-            {
-                mProgressContainer.startAnimation(AnimationUtils.loadAnimation(
-                        getActivity(), android.R.anim.fade_out));
-                mListContainer.startAnimation(AnimationUtils.loadAnimation(
-                        getActivity(), android.R.anim.fade_in));
-            }
-            mProgressContainer.setVisibility(View.GONE);
-            mListContainer.setVisibility(View.VISIBLE);
-        }
-        else
-        {
-            if (animate)
-            {
-                mProgressContainer.startAnimation(AnimationUtils.loadAnimation(
-                        getActivity(), android.R.anim.fade_in));
-                mListContainer.startAnimation(AnimationUtils.loadAnimation(
-                        getActivity(), android.R.anim.fade_out));
-            }
-            mProgressContainer.setVisibility(View.VISIBLE);
-            mListContainer.setVisibility(View.INVISIBLE);
-        }
-    }
-
-    public void setListShown(boolean shown)
-    {
-        setListShown(shown, true);
-    }
-
-    public void setListShownNoAnimation(boolean shown)
-    {
-        setListShown(shown, false);
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
-    {
-        int INTERNAL_EMPTY_ID = 0x00ff0001;
-        View root = inflater.inflate(R.layout.ap_selector_fragment, container, false);
-        (root.findViewById(R.id.internalEmpty)).setId(INTERNAL_EMPTY_ID);
-        mList = (ListView) root.findViewById(android.R.id.list);
-        mListContainer = root.findViewById(R.id.listContainer);
-        mProgressContainer = root.findViewById(R.id.progressContainer);
-        mListShown = true;
-        return root;
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.ap_selector_fragment, container, false);
     }
 
-    public static AccessPointListFragment getInstance()
-    {
+    public static AccessPointListFragment getInstance() {
         if (instance == null)
             instance = new AccessPointListFragment();
 
@@ -103,210 +42,19 @@ public class AccessPointListFragment extends ListFragment implements LoaderManag
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState)
-    {
-        super.onActivityCreated(savedInstanceState);
-
-
-        // Initially there is no data
-//        setEmptyText("No Data Here");
-
-        // Create an empty adapter we will use to display the loaded data.
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         apListAdapter = new ProxySelectorListAdapter(getActivity());
         setListAdapter(apListAdapter);
-
-        // Start out with a progress indicator.
-        setListShown(false);
-
-        // Prepare the loader.  Either re-connect with an existing one,
-        // or start a new one.
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-//    {
-//        View v = inflater.inflate(R.layout.ap_selector_fragment, null);
-//        return v;
-//    }
-
-    @Override
-    public Loader<List<ProxyConfiguration>> onCreateLoader(int arg0, Bundle arg1)
-    {
-        return new DataListLoader(getActivity());
     }
 
     @Override
-    public void onLoadFinished(Loader<List<ProxyConfiguration>> arg0, List<ProxyConfiguration> data)
-    {
-        apListAdapter.setData(data);
-
-        // The list should now be shown.
-        if (isResumed())
-        {
-            setListShown(true);
-        }
-        else
-        {
-            setListShownNoAnimation(true);
-        }
-    }
-
-    @Override
-    public void onLoaderReset(Loader<List<ProxyConfiguration>> arg0)
-    {
-        apListAdapter.setData(null);
-    }
-
-    public static class DataListLoader extends AsyncTaskLoader<List<ProxyConfiguration>>
-    {
-        List<ProxyConfiguration> mModels;
-
-        public DataListLoader(Context context)
-        {
-            super(context);
-        }
-
-        @Override
-        public List<ProxyConfiguration> loadInBackground()
-        {
-            // You should perform the heavy task of getting data from
-            // Internet or database or other source
-            // Here, we are generating some Sample data
-
-            // Create corresponding array of entries and load with data.
-            List<ProxyConfiguration> entries = ApplicationGlobals.getConfigurationsList();
-            return entries;
-        }
-
-        /**
-         * Called when there is new data to deliver to the client.  The
-         * super class will take care of delivering it; the implementation
-         * here just adds a little more logic.
-         */
-        @Override
-        public void deliverResult(List<ProxyConfiguration> listOfData)
-        {
-            if (isReset())
-            {
-                // An async query came in while the loader is stopped.  We
-                // don't need the result.
-                if (listOfData != null)
-                {
-                    onReleaseResources(listOfData);
-                }
-            }
-            List<ProxyConfiguration> oldApps = listOfData;
-            mModels = listOfData;
-
-            if (isStarted())
-            {
-                // If the Loader is currently started, we can immediately
-                // deliver its results.
-                super.deliverResult(listOfData);
-            }
-
-            // At this point we can release the resources associated with
-            // 'oldApps' if needed; now that the new result is delivered we
-            // know that it is no longer in use.
-            if (oldApps != null)
-            {
-                onReleaseResources(oldApps);
-            }
-        }
-
-        /**
-         * Handles a request to start the Loader.
-         */
-        @Override
-        protected void onStartLoading()
-        {
-            if (mModels != null)
-            {
-                // If we currently have a result available, deliver it
-                // immediately.
-                deliverResult(mModels);
-            }
-
-
-            if (takeContentChanged() || mModels == null)
-            {
-                // If the data has changed since the last time it was loaded
-                // or is not currently available, start a load.
-                forceLoad();
-            }
-        }
-
-        /**
-         * Handles a request to stop the Loader.
-         */
-        @Override
-        protected void onStopLoading()
-        {
-            // Attempt to cancel the current load task if possible.
-            cancelLoad();
-        }
-
-        /**
-         * Handles a request to cancel a load.
-         */
-        @Override
-        public void onCanceled(List<ProxyConfiguration> apps)
-        {
-            super.onCanceled(apps);
-
-            // At this point we can release the resources associated with 'apps'
-            // if needed.
-            onReleaseResources(apps);
-        }
-
-        /**
-         * Handles a request to completely reset the Loader.
-         */
-        @Override
-        protected void onReset()
-        {
-            super.onReset();
-
-            // Ensure the loader is stopped
-            onStopLoading();
-
-            // At this point we can release the resources associated with 'apps'
-            // if needed.
-            if (mModels != null)
-            {
-                onReleaseResources(mModels);
-                mModels = null;
-            }
-        }
-
-        /**
-         * Helper function to take care of releasing resources associated
-         * with an actively loaded data set.
-         */
-        protected void onReleaseResources(List<ProxyConfiguration> apps)
-        {
-        }
-
-    }
-
-
-    @Override
-    public void onSaveInstanceState(Bundle outState)
-    {
-        super.onSaveInstanceState(outState);
-        outState.putInt("curChoice", mCurCheckPosition);
-    }
-
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id)
-    {
+    public void onListItemClick(ListView l, View v, int position, long id) {
         showDetails(position);
     }
 
     @Override
-    public void onResume()
-    {
+    public void onResume() {
         super.onResume();
 
         refreshUI();
@@ -318,13 +66,10 @@ public class AccessPointListFragment extends ListFragment implements LoaderManag
 //        StatusFragment.getInstance().Hide();
     }
 
-    public void refreshUI()
-    {
-        if (apListAdapter != null)
-            apListAdapter.clear();
-
-//        final ArrayList<ProxyConfiguration> confsList = (ArrayList<ProxyConfiguration>) ApplicationGlobals.getConfigurationsList();
-//        apListAdapter = new ProxySelectorListAdapter(getActivity(), R.layout.ap_selector_fragment, confsList);
+    public void refreshUI() {
+        if (apListAdapter != null) {
+            apListAdapter.setData(ApplicationGlobals.getConfigurationsList());
+        }
     }
 
     /**
@@ -332,8 +77,7 @@ public class AccessPointListFragment extends ListFragment implements LoaderManag
      * displaying a fragment in-place in the current UI, or starting a
      * whole new activity in which it is displayed.
      */
-    void showDetails(int index)
-    {
+    void showDetails(int index) {
         mCurCheckPosition = index;
 
         // We can display everything in-place with fragments, so update
@@ -349,12 +93,9 @@ public class AccessPointListFragment extends ListFragment implements LoaderManag
         // Execute a transaction, replacing any existing fragment
         // with this one inside the frame.
         FragmentTransaction ft = getFragmentManager().beginTransaction();
-        if (index == 0)
-        {
+        if (index == 0) {
             ft.replace(R.id.fragment_container, details);
-        }
-        else
-        {
+        } else {
             // TODO Check here
             ft.replace(R.id.fragment_container, details);
         }
