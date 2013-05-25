589006991f41e7632454f8bb164048f2d86580a9
lechuckcaptain <lechuckcaptain@shouldit.net>
2013-05-25 12:28:32 +0200

    Added new bugsense library Fixed check of current network status

--- src/com/lechucksoftware/proxy/proxysettings/services/ProxySettingsCheckerService.java
+++ src/com/lechucksoftware/proxy/proxysettings/services/ProxySettingsCheckerService.java
@@ -15,100 +15,105 @@ import com.lechucksoftware.proxy.proxysettings.utils.UIUtils;
 import com.shouldit.proxy.lib.APLConstants;
 import com.shouldit.proxy.lib.ProxyConfiguration;
 
+import java.util.Date;
+
 public class ProxySettingsCheckerService extends IntentService
 {
-	public static final String CALLER_INTENT = "CallerIntent";
-	public static String TAG = "ProxySettingsCheckerService";
+    public static final String CALLER_INTENT = "CallerIntent";
+    public static String TAG = "ProxySettingsCheckerService";
 
-	public ProxySettingsCheckerService()
-	{
-		super("ProxySettingsCheckerService");
-	}
+    public ProxySettingsCheckerService()
+    {
+        super("ProxySettingsCheckerService");
+    }
 
-	@Override
-	protected void onHandleIntent(Intent intent)
-	{
-		Intent callerIntent = (Intent) intent.getExtras().get(CALLER_INTENT);
+    @Override
+    protected void onHandleIntent(Intent intent)
+    {
+        Intent callerIntent = (Intent) intent.getExtras().get(CALLER_INTENT);
 
-		if (callerIntent != null)
-		{
-			String callerAction = callerIntent.getAction();
-			LogWrapper.logIntent(TAG, callerIntent, Log.DEBUG);
+        if (callerIntent != null)
+        {
+            String callerAction = callerIntent.getAction();
+            LogWrapper.logIntent(TAG, callerIntent, Log.DEBUG);
 
-			if (   callerAction.equals(Constants.PROXY_SETTINGS_STARTED) 
-				|| callerAction.equals(Constants.PROXY_SETTINGS_MANUAL_REFRESH)
-				|| callerAction.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
-				|| callerAction.equals(APLConstants.APL_UPDATED_PROXY_CONFIGURATION))
-			{
-				// LogWrapper.logIntent(TAG, callerIntent, Log.WARN);
-				CheckProxySettings(callerIntent);
-			}	
-			else if (callerAction.equals(ConnectivityManager.CONNECTIVITY_ACTION))
-			{
-				Boolean noConnectivity = callerIntent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
-				if (noConnectivity)
-				{
-					return;
-				}
+            if (callerAction.equals(Constants.PROXY_SETTINGS_STARTED)
+                    || callerAction.equals(Constants.PROXY_SETTINGS_MANUAL_REFRESH)
+                    || callerAction.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+                    || callerAction.equals(APLConstants.APL_UPDATED_PROXY_CONFIGURATION))
+            {
+                // LogWrapper.logIntent(TAG, callerIntent, Log.WARN);
+                CheckProxySettings(callerIntent);
+            }
+            else if (callerAction.equals(ConnectivityManager.CONNECTIVITY_ACTION))
+            {
+                Boolean noConnectivity = callerIntent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+                if (noConnectivity)
+                {
+                    return;
+                }
 
                 //TODO : check here
-				//int intentNetworkType = callerIntent.getIntExtra(ConnectivityManager.EXTRA_NETWORK_INFO , -1);
-				NetworkInfo ni = ApplicationGlobals.getConnectivityManager().getActiveNetworkInfo();
+                //int intentNetworkType = callerIntent.getIntExtra(ConnectivityManager.EXTRA_NETWORK_INFO , -1);
+                NetworkInfo ni = ApplicationGlobals.getConnectivityManager().getActiveNetworkInfo();
 
-				if (ni != null && ni.isConnected())
-				{
-					//if (ni.getType() == intentNetworkType) // Check only for
-														   // intent related to
-														   // active network
-					{
-						// LogWrapper.logIntent(TAG, callerIntent, Log.DEBUG,
-						// true);
-						CheckProxySettings(callerIntent);
-					}
-				}
-				else
-				{
-					LogWrapper.d(TAG,"Do not check proxy settings if network is not available!");
-				}
-				// else
-				// LogWrapper.logIntent(TAG, callerIntent, Log.DEBUG, false);
-			}
-			else
-			{
-				LogWrapper.e(TAG, "Intent ACTION not handled: " + callerAction);
-			}
-		}
-		else
-		{
-			LogWrapper.e(TAG, "Received Intent NULL ACTION");
-		}
-	}
+                if (ni != null && ni.isConnected())
+                {
+                    //if (ni.getType() == intentNetworkType) // Check only for
+                    // intent related to
+                    // active network
+                    {
+                        // LogWrapper.logIntent(TAG, callerIntent, Log.DEBUG,
+                        // true);
+                        CheckProxySettings(callerIntent);
+                    }
+                }
+                else
+                {
+                    LogWrapper.d(TAG, "Do not check proxy settings if network is not available!");
+                }
+                // else
+                // LogWrapper.logIntent(TAG, callerIntent, Log.DEBUG, false);
+            }
+            else
+            {
+                LogWrapper.e(TAG, "Intent ACTION not handled: " + callerAction);
+            }
+        }
+        else
+        {
+            LogWrapper.e(TAG, "Received Intent NULL ACTION");
+        }
+    }
 
-	@Override
-	public void onDestroy()
-	{
-		LogWrapper.d(TAG, "ProxySettingsCheckerService destroying");
-	};
+    @Override
+    public void onDestroy()
+    {
+        LogWrapper.d(TAG, "ProxySettingsCheckerService destroying");
+    }
 
-	public void CheckProxySettings(Intent callerIntent)
-	{
-		try
-		{
-			ProxyConfiguration oldconf = null;
+    ;
 
-			if (!callerIntent.getAction().equals(Constants.PROXY_SETTINGS_STARTED))
-			{
-				oldconf = ApplicationGlobals.getInstance().getCachedConfiguration();
-			}	
+    public void CheckProxySettings(Intent callerIntent)
+    {
+        try
+        {
+            ProxyConfiguration oldconf = null;
 
-			CallRefreshApplicationStatus();
-			
-			ApplicationGlobals.getInstance().updateProxyConfigurationList();
-			ProxyConfiguration newconf = ApplicationGlobals.getInstance().getCurrentConfiguration();
+            if (!callerIntent.getAction().equals(Constants.PROXY_SETTINGS_STARTED))
+            {
+                oldconf = ApplicationGlobals.getInstance().getCachedConfiguration();
+            }
 
-			NetworkInfo ni = ApplicationGlobals.getConnectivityManager().getActiveNetworkInfo();
-			if (ni != null && ni.isAvailable() && ni.isConnected())
-			{
+            CallRefreshApplicationStatus();
+
+            ApplicationGlobals.getInstance().updateProxyConfigurationList();
+            ProxyConfiguration newconf = ApplicationGlobals.getInstance().getCurrentConfiguration();
+
+            NetworkInfo ni = ApplicationGlobals.getConnectivityManager().getActiveNetworkInfo();
+
+            if (ni != null && ni.isAvailable() && ni.isConnected())
+            {
                 boolean checkNewConf = false;
                 if (newconf != null)
                 {
@@ -120,7 +125,21 @@ public class ProxySettingsCheckerService extends IntentService
                         }
                         else
                         {
-                            // Skip check when configuration is the same
+                            if (oldconf.status != null
+                                    && oldconf.status.checkedDate != null)
+                            {
+
+                                long diffMsec = new Date().getTime() - oldconf.status.checkedDate.getTime();
+                                long diffSeconds = diffMsec / 1000;
+                                long diffMinutes = diffMsec / (60 * 1000);
+
+                                if (diffMinutes > 30)
+                                {
+                                    checkNewConf = true;
+                                    // Skip check when configuration is the same
+                                    LogWrapper.d(TAG, "Same configuration for 30 minutes check again!");
+                                }
+                            }
                         }
                     }
                     else
@@ -137,30 +156,37 @@ public class ProxySettingsCheckerService extends IntentService
                 if (checkNewConf)
                 {
                     LogWrapper.d(TAG, "Changed current proxy configuration: calling refresh of proxy status");
-					newconf.acquireProxyStatus(ApplicationGlobals.getInstance().timeout);
-					LogWrapper.i(TAG, newconf.toString());
-				}
-			}
+                    newconf.acquireProxyStatus(ApplicationGlobals.getInstance().timeout);
+                    LogWrapper.i(TAG, newconf.toString());
+                }
+                else
+                {
+                    // Skip check when configuration is the same
+                    LogWrapper.d(TAG, "Same configuration, skip checking...");
+                }
+            }
 
-			CallRefreshApplicationStatus();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-			UIUtils.DisableProxyNotification(getApplicationContext());
-			LogWrapper.d(TAG, "Exception caught: disable proxy notification");
-		}
-	}
+//            TEMP DISABLED
+//            CallRefreshApplicationStatus();
+        }
+        catch (Exception e)
+        {
+            BugSenseHandler.sendException(e);
+            UIUtils.DisableProxyNotification(getApplicationContext());
+            LogWrapper.d(TAG, "Exception caught: disable proxy notification");
+        }
 
-	public void CallRefreshApplicationStatus()
-	{
-		/**
-		 * Call the update of the UI
-		 * */
-		LogWrapper.d(TAG, "Sending broadcast intent " + Constants.PROXY_REFRESH_UI);
-		Intent intent = new Intent(Constants.PROXY_REFRESH_UI);
-		getApplicationContext().sendBroadcast(intent);
+    }
 
-		UIUtils.UpdateStatusBarNotification(ApplicationGlobals.getInstance().getCachedConfiguration(), getApplicationContext());
-	}
+    public void CallRefreshApplicationStatus()
+    {
+        /**
+         * Call the update of the UI
+         * */
+        LogWrapper.d(TAG, "Sending broadcast intent " + Constants.PROXY_REFRESH_UI);
+        Intent intent = new Intent(Constants.PROXY_REFRESH_UI);
+        getApplicationContext().sendBroadcast(intent);
+
+        UIUtils.UpdateStatusBarNotification(ApplicationGlobals.getInstance().getCachedConfiguration(), getApplicationContext());
+    }
 }
