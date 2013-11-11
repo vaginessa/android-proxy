61c5b8d7cfb59ea5de998e03969c3f52e9d1dffe
lechuckcaptain <lechuckcaptain@shouldit.net>
2013-11-11 23:06:57 +0100

    Fixed layout and UI refresh of tagspreference

--- src/com/lechucksoftware/proxy/proxysettings/preferences/TagsPreference.java
+++ src/com/lechucksoftware/proxy/proxysettings/preferences/TagsPreference.java
@@ -1,54 +1,64 @@
 package com.lechucksoftware.proxy.proxysettings.preferences;
 
 import android.content.Context;
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
 import android.preference.DialogPreference;
-import android.preference.Preference;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.ImageView;
-
+import android.widget.TextView;
 import com.lechucksoftware.proxy.proxysettings.R;
 import com.lechucksoftware.proxy.proxysettings.components.TagsView;
 import com.lechucksoftware.proxy.proxysettings.db.DBTag;
-import com.shouldit.proxy.lib.CheckStatusValues;
-import com.shouldit.proxy.lib.ProxyStatusItem;
 
 import java.util.List;
 
 public class TagsPreference extends DialogPreference
 {
     private TagsView tagsView;
-    private List<DBTag> _tags;
+    private List<DBTag> tags;
+    private TextView summary;
 
     public TagsPreference(Context context, AttributeSet attrs)
-	{
-		super(context, attrs);
+    {
+        super(context, attrs);
         setLayoutResource(R.layout.tags_preference);
         setWidgetLayoutResource(android.R.string.ok);
         setWidgetLayoutResource(android.R.string.cancel);
         setDialogIcon(null);
-	}
+    }
 
     @Override
     protected void onBindView(View view)
     {
         super.onBindView(view);
 
+        summary = (TextView) view.findViewById(android.R.id.summary);
         tagsView = (TagsView) view.findViewById(R.id.preference_proxy_tags);
+
         refreshUI();
     }
 
-    public void setTags(List<DBTag> tags)
+    public void setTags(List<DBTag> intags)
     {
-        _tags = tags;
+        tags = intags;
         refreshUI();
     }
 
     private void refreshUI()
     {
-        if (tagsView != null && _tags != null)
-            tagsView.setTags(_tags);
+        if (tagsView != null)
+        {
+            if (tags != null)
+            {
+                tagsView.setTags(tags);
+                tagsView.setVisibility(View.VISIBLE);
+                summary.setVisibility(View.GONE);
+            }
+            else
+            {
+                tagsView.setVisibility(View.GONE);
+                summary.setVisibility(View.VISIBLE);
+                summary.setText(R.string.not_set);
+            }
+        }
     }
 }
