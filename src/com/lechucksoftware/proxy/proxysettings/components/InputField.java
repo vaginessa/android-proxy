f42998c31b1dd2e3601830928769154ff4a08c1e
lechuckcaptain <lechuckcaptain@shouldit.net>
2013-12-08 14:56:21 +0100

    Working on style of inputtags

--- src/com/lechucksoftware/proxy/proxysettings/components/InputField.java
+++ src/com/lechucksoftware/proxy/proxysettings/components/InputField.java
@@ -2,6 +2,7 @@ package com.lechucksoftware.proxy.proxysettings.components;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.text.InputType;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +24,7 @@ public class InputField extends LinearLayout
     private String value;
     private boolean readonly;
     private boolean fullsize;
+    private int type;
 
     public InputField(Context context, AttributeSet attrs)
     {
@@ -35,47 +37,17 @@ public class InputField extends LinearLayout
 
         View v = inflater.inflate(R.layout.input, this);
 
-        if (inflater != null)
+        if (v != null)
         {
             titleTextView = (TextView) v.findViewById(R.id.field_title);
             valueReadOnlyTextView = (TextView) v.findViewById(R.id.field_value_readonly);
             valueEditText = (EditText) v.findViewById(R.id.field_value);
 
-            if (fullsize || readonly)
-            {
-                titleTextView.setText(title);
-                titleTextView.setVisibility(VISIBLE);
-            }
-            else
-            {
-                titleTextView.setVisibility(GONE);
-            }
-
-            if (value != null && value.length() > 0)
-            {
-                valueReadOnlyTextView.setText(value);
-                valueEditText.setText(value);
-            }
-            else
-            {
-                valueEditText.setHint(hint);
-                valueReadOnlyTextView.setText("NOT SET");
-            }
-
-            if (readonly)
-            {
-                valueReadOnlyTextView.setVisibility(VISIBLE);
-                valueEditText.setVisibility(GONE);
-            }
-            else
-            {
-                valueEditText.setVisibility(VISIBLE);
-                valueReadOnlyTextView.setVisibility(GONE);
-            }
+            refreshUI();
         }
     }
 
-    private void readStyleParameters(Context context, AttributeSet attributeSet)
+    protected void readStyleParameters(Context context, AttributeSet attributeSet)
     {
         TypedArray a = context.obtainStyledAttributes(attributeSet, R.styleable.InputField);
 
@@ -86,10 +58,58 @@ public class InputField extends LinearLayout
             value = a.getString(R.styleable.InputField_value);
             readonly = a.getBoolean(R.styleable.InputField_readonly, false);
             fullsize = a.getBoolean(R.styleable.InputField_fullsize, false);
+            type = a.getInt(R.styleable.InputField_inputType, 0);
         }
         finally
         {
             a.recycle();
+        }
+    }
+
+    public void refreshUI()
+    {
+        if (fullsize || readonly)
+        {
+            titleTextView.setText(title);
+            titleTextView.setVisibility(VISIBLE);
+        }
+        else
+        {
+            titleTextView.setVisibility(GONE);
+        }
+
+        if (value != null && value.length() > 0)
+        {
+            valueReadOnlyTextView.setText(value);
+            valueEditText.setText(value);
+        }
+        else
+        {
+            valueEditText.setHint(hint);
+            valueReadOnlyTextView.setText("NOT SET");
+        }
+
+        if (readonly)
+        {
+            valueReadOnlyTextView.setVisibility(VISIBLE);
+            valueEditText.setVisibility(GONE);
+        }
+        else
+        {
+            valueEditText.setVisibility(VISIBLE);
+            valueReadOnlyTextView.setVisibility(GONE);
+        }
+
+        switch (type)
+        {
+            case 1:
+                valueEditText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                break;
+
+            case 0:
+            default:
+                valueEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+                break;
         }
     }
 }
---
+++ src/com/lechucksoftware/proxy/proxysettings/components/InputTags.java
@@ -0,0 +1,86 @@
+package com.lechucksoftware.proxy.proxysettings.components;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.lechucksoftware.proxy.proxysettings.R;
+import com.lechucksoftware.proxy.proxysettings.db.TagEntity;
+
+import java.util.List;
+
+/**
+ * Created by Marco on 08/12/13.
+ */
+public class InputTags extends LinearLayout
+{
+    private Button addTagsButton;
+    private TagsView tagsView;
+//    private TextView titleTextView;
+    private String title;
+    private boolean fullsize;
+    private List<TagEntity> tags;
+
+    public InputTags(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+
+        readStyleParameters(context,attrs);
+
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View v = inflater.inflate(R.layout.input_tags, this);
+
+        if (v != null)
+        {
+//            titleTextView = (TextView) v.findViewById(R.id.field_title);
+            addTagsButton = (Button) v.findViewById(R.id.field_add_tags);
+
+
+
+            tagsView = (TagsView) v.findViewById(R.id.field_tags);
+        }
+    }
+
+    public void setTags(List<TagEntity> intags)
+    {
+        tags = intags;
+        refreshUI();
+    }
+
+    private void refreshUI()
+    {
+//        titleTextView.setText(title);
+
+        tagsView.setTags(tags);
+        if (tags != null && tags.size() > 0)
+        {
+            tagsView.setVisibility(VISIBLE);
+            addTagsButton.setVisibility(GONE);
+        }
+        else
+        {
+            tagsView.setVisibility(GONE);
+            addTagsButton.setVisibility(VISIBLE);
+        }
+    }
+
+    protected void readStyleParameters(Context context, AttributeSet attributeSet)
+    {
+        TypedArray a = context.obtainStyledAttributes(attributeSet, R.styleable.InputFieldTags);
+
+        try
+        {
+            title = a.getString(R.styleable.InputField_title);
+            fullsize = a.getBoolean(R.styleable.InputField_fullsize, false);
+        }
+        finally
+        {
+            a.recycle();
+        }
+    }
+}
