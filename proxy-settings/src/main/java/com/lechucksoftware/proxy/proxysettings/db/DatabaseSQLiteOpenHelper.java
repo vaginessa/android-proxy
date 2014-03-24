0db2cd4642c7c8af98157da67d5bbd100166bc0a
lechuckcaptain <lechuckcaptain@shouldit.net>
2014-03-24 23:24:51 +0100

    Restored DB version, used only for tests

--- proxy-settings/src/main/java/com/lechucksoftware/proxy/proxysettings/db/DatabaseSQLiteOpenHelper.java
+++ proxy-settings/src/main/java/com/lechucksoftware/proxy/proxysettings/db/DatabaseSQLiteOpenHelper.java
@@ -33,7 +33,7 @@ public class DatabaseSQLiteOpenHelper extends SQLiteOpenHelper
     public static final String COLUMN_TAG_ID = "tagId";
 
     public static final String DATABASE_NAME = "proxysettings.db";
-    public static final int DATABASE_VERSION = 3;
+    public static final int DATABASE_VERSION = 2;
 
     // Database creation sql statement
 
