8f2c2ec2ed96b0ed8af70c24eed59b2a0758d57f
lechuckcaptain <lechuckcaptain@gmail.com>
2014-12-16 17:08:25 +0100

    Adding Espresso test

--- proxy-settings/src/androidTest/java/com/lechucksoftware/proxy/proxysettings/test/ApplicationTest.java
+++ proxy-settings/src/androidTest/java/com/lechucksoftware/proxy/proxysettings/test/ApplicationTest.java
@@ -1,43 +1,43 @@
-package com.lechucksoftware.proxy.proxysettings.test;
-
-import android.test.ApplicationTestCase;
-import android.test.suitebuilder.annotation.SmallTest;
-
-import com.lechucksoftware.proxy.proxysettings.App;
-
-import java.util.List;
-
-import be.shouldit.proxy.lib.WiFiAPConfig;
-
-/**
- * Created by mpagliar on 22/08/2014.
- */
-public class ApplicationTest extends ApplicationTestCase<App>
-{
-    public ApplicationTest()
-    {
-        super(App.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception
-    {
-        super.setUp();
-        createApplication();
-    }
-
-    @SmallTest
-    public void testWifiNetworksManagerStartup() throws Exception
-    {
-        List<WiFiAPConfig> result = App.getWifiNetworksManager().getSortedWifiApConfigsList();
-        assertNotNull(result);
-        assertTrue(result.size() > 0);
-    }
-
-    @Override
-    protected void tearDown() throws Exception
-    {
-        super.tearDown();
-        terminateApplication();
-    }
-}
+//package com.lechucksoftware.proxy.proxysettings.test;
+//
+//import android.test.ActivityInstrumentationTestCase2;
+//
+//import com.google.android.apps.common.testing.ui.espresso.Espresso;
+//import com.google.android.apps.common.testing.ui.espresso.action.ViewActions;
+//import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
+//import com.lechucksoftware.proxy.proxysettings.R;
+//import com.lechucksoftware.proxy.proxysettings.ui.activities.MasterActivity;
+//
+///**
+// * Created by mpagliar on 22/08/2014.
+// */
+//
+//public class ApplicationTest extends ActivityInstrumentationTestCase2<MasterActivity>
+//{
+//    public ApplicationTest()
+//    {
+//        super(MasterActivity.class);
+//    }
+//
+//    @Override
+//    public void setUp() throws Exception
+//    {
+//        super.setUp();
+//        getActivity();
+//    }
+//
+//    public void testEspressoClickingListViewPopulatesTextView()
+//    {
+//        //Given the ListView is populated
+//        Espresso.onView(ViewMatchers.withText(R.string.wifi_access_points)).perform(ViewActions.click());
+//
+////        // When I click on the an item in the ListView
+////        Espresso.onData(Matchers.allOf(Matchers.is(
+////                Matchers.instanceOf(String.class)), Matchers.is("Grape")))
+////                .perform(ViewActions.click());
+////
+////        // Then the TextView shows the correct text
+////        Espresso.onView(ViewMatchers.withId(R.id.my_hello_text_view))
+////                .check(ViewAssertions.matches(ViewMatchers.withText(Matchers.containsString("Grape"))));
+//    }
+//}
